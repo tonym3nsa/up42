72141e7aa62d7fffcba285991dc724ca93822b2c
Anthony Agyekum Mensah <tony.m3nsa@gmail.com>
2022-09-11 01:18:10 +0000

    refactor: fix constant string

--- src/redux/actions/blocks.js
+++ src/redux/actions/blocks.js
@@ -1,6 +1,6 @@
 export const GET_BLOCKS = "BLOCKS:GET_BLOCKS";
 export const GETTING_BLOCKS = "BLOCKS:GETTING_BLOCKS";
-export const ERROR_GETTING_BLOCKS = "error?.message:ERROR_GETTING_BLOCKS";
+export const ERROR_GETTING_BLOCKS = "BLOCKS:ERROR_GETTING_BLOCKS";
 import axios from "axios";
 
 export const getBlocks = () => {
